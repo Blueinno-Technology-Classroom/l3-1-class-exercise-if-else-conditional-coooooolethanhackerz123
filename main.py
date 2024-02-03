99f6ac181d9a8caa13a7f0b489d0524675e0ded7
cooooooolethanhackerz22113 <wangethanhk@gmail.com>
2024-02-03 12:14:24 +0800

    1

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 ##################################################
 '''
 Q1:
-a: 
-b:  
-c:  
-d: 
-e: 
-f: 
-g: 
-h: 
-i: 
+a: True
+b: False
+c: True 
+d: False
+e: true
+f: false
+g: true
+h: true
+i: true
 '''
 
 ##################################################
 '''
 Q2:
-a:  
-b:  
-c:  
-d: 
-e: 
-f: 
-g: 
-h: 
-i: 
-j: 
+a:  y<0
+b:  z>0
+c:  z>y
+d: z>0 or z==0
+e: x==z
+f: z%2=1
+g: x==4
+h: y/z == 0
+i: y<0 or y==0
+j: x==z
 k: 
 '''
 
@@ -39,6 +39,6 @@ if number % 2 == 0:
     if number % 3 == 0:
         print("Divisible by 6.")
     else:
-        print("Odd number.")
+        print("Even number.")
 
 ##################################################
